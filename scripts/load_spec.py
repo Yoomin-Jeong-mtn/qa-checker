c7ed742d735fe4cc5f1bab46164d4e0e44f8b852
Yoomin Jeong <yoomin.jeong@martinee.io>
2026-04-17 19:22:10 +0900

    feat: add spec loader with extends support

---
+++ scripts/load_spec.py
@@ -0,0 +1,40 @@
+import json
+import sys
+from pathlib import Path
+
+import yaml
+
+
+def load_spec(specs_dir: str) -> dict:
+    specs_path = Path(specs_dir)
+
+    common_groups: dict = {}
+    common_path = specs_path / '_common.yaml'
+    if common_path.exists():
+        with open(common_path, encoding='utf-8') as f:
+            common_data = yaml.safe_load(f)
+        common_groups = common_data.get('properties', {})
+
+    spec: dict = {}
+    for yaml_file in specs_path.glob('*.yaml'):
+        if yaml_file.name.startswith('_'):
+            continue
+        with open(yaml_file, encoding='utf-8') as f:
+            event_data = yaml.safe_load(f)
+
+        event_name = event_data['event']
+        props: list = []
+
+        extends = event_data.get('extends')
+        if extends and extends in common_groups:
+            props.extend(common_groups[extends])
+
+        props.extend(event_data.get('properties') or [])
+        spec[event_name] = {p['key']: p for p in props}
+
+    return spec
+
+
+if __name__ == '__main__':
+    result = load_spec(sys.argv[1])
+    print(json.dumps(result, ensure_ascii=False))
